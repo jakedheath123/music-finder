0567039ee1a7d84190a838a609aaf5276f0b6b9c
jakedheath123 <jakedheath@gmail.com>
2020-06-29 19:00:07 +0100

    add home page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./components/HomePage/HomePage";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
