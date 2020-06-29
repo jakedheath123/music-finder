46774b5618d3567d6ba7561fc109a7ff6044da8a
jakedheath123 <jakedheath@gmail.com>
2020-06-29 20:55:13 +0100

    create artist page component and add to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Router } from "@reach/router";
 
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
+import ArtistPage from "./components/ArtistPage/ArtistPage";
 
 function App() {
   return (
     <>
       <Router primary={false}>
         <HomePage path="/" />
+        <ArtistPage path="/artist:artist_id" />
       </Router>
     </>
   );
