e89fdecffc90ae8ad11849bdb0f4e9930b48aeb4
jakedheath123 <jakedheath@gmail.com>
2020-06-29 15:38:36 +0100

    install and add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 
 const schema = require("./schema");
 
 const app = express();
 
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -13,6 +15,6 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
