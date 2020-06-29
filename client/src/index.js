9c958c0f78c134cff25357b89a458e46e66268ae
jakedheath123 <jakedheath@gmail.com>
2020-06-29 15:50:17 +0100

    create client for data requesting

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { ApolloClient } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
