c0a512c0df07715cb5a8b3507152b53b22cfabfb
jakedheath123 <jakedheath@gmail.com>
2020-07-01 15:47:31 +0100

    add title, preview and contributors information from track data

--- client/src/components/ArtistTopTracks/ArtistTopTracks.jsx
+++ client/src/components/ArtistTopTracks/ArtistTopTracks.jsx
@@ -2,16 +2,43 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
-const ArtistTopTracks = () => {
+const ArtistTopTracks = ({ artist_id }) => {
+  const ARTIST_TOP_TRACKS_QUERY = gql`
+  query {
+    topArtistTracks(id: "${artist_id}") {
+      title_short,
+      preview,
+      contributors {
+        name
+      }
+    }
+  }
+  `;
+
+  const { loading, data } = useQuery(ARTIST_TOP_TRACKS_QUERY);
+
+  if (loading) return <p>Loading</p>;
+
+  const { topArtistTracks } = data;
+
   return (
-    <div>
-      {/* <audio controls>
-        <source
-          src=""
-          type="audio/ogg"
-        />
-      </audio> */}
-    </div>
+    <ul>
+      {topArtistTracks.map(track => {
+        const { title_short, preview, contributors } = track;
+        return (
+          <li key={title_short}>
+            <h3>{title_short}</h3>
+            <audio controls>
+              <source src={preview} type="audio/ogg" />
+            </audio>
+            {contributors.map((contributor, index) => {
+              const { name } = contributor;
+              return <p key={index}>{name}</p>;
+            })}
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
