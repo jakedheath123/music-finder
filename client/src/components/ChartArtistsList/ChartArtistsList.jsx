6ddd7109e2e61d5b94db8e50806035986ea1e223
jakedheath123 <jakedheath@gmail.com>
2020-07-01 12:11:44 +0100

    create ChartArtistList component

---
+++ client/src/components/ChartArtistsList/ChartArtistsList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartArtistsList = () => {
+  return <div>CHART</div>;
+};
+
+export default ChartArtistsList;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { Link } from "@reach/router";
 
 import "./Home.css";
 import SearchForm from "../SearchForm/SearchForm";
+import ChartArtistsList from "../ChartArtistsList/ChartArtistsList";
 
 const HomePage = () => {
   const [artistSearch, setArtistSearch] = useState(null);
@@ -31,20 +32,23 @@ const HomePage = () => {
   const { name, picture_big, id } = data.artist;
 
   return (
-    <main className="home-container">
-      <div className="home-search">
-        <SearchForm searchByArtist={searchByArtist} />
-      </div>
-      <div className="home-content">
-        {picture_big ? (
-          <Link to={`/artist/${id}`}>
-            {" "}
-            <img src={picture_big} alt="Artist cover" />{" "}
-          </Link>
-        ) : null}
-        <h1>{name}</h1>
-      </div>
-    </main>
+    <>
+      <ChartArtistsList />
+      <main className="home-container">
+        <div className="home-search">
+          <SearchForm searchByArtist={searchByArtist} />
+        </div>
+        <div className="home-content">
+          {picture_big ? (
+            <Link to={`/artist/${id}`}>
+              {" "}
+              <img src={picture_big} alt="Artist cover" />{" "}
+            </Link>
+          ) : null}
+          <h1>{name}</h1>
+        </div>
+      </main>
+    </>
   );
 };
 
