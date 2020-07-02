38430ef68fbac7223e72b5dca0ebf53a8c77bccd
jakedheath123 <jakedheath@gmail.com>
2020-07-02 16:28:21 +0100

    add album tracks query to single album component to display information on each track in an album

--- client/src/components/SingleAlbum/SingleAlbum.jsx
+++ client/src/components/SingleAlbum/SingleAlbum.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-const SingleAlbum = () => {
-  return <div>ALBUM</div>;
+const SingleAlbum = ({ album_id }) => {
+  const ALBUM_TRACKS_QUERY = gql`
+  query albumTracksQuery {
+    albumTracks(id: ${album_id}) {
+      title_short
+      track_position
+      explicit_lyrics
+      preview
+    }
+  }
+  `;
+
+  const { loading, data } = useQuery(ALBUM_TRACKS_QUERY);
+
+  if (loading) return <p>Loading</p>;
+
+  const { albumTracks } = data;
+
+  return (
+    <main>
+      <ul>
+        {albumTracks.map(track => {
+          const {
+            title_short,
+            track_position,
+            explicit_lyrics,
+            preview
+          } = track;
+          return (
+            <li>
+              <h1>{title_short}</h1>
+              <h2>{track_position}</h2>
+              <h3>{explicit_lyrics}</h3>
+              <audio controls>
+                <source src={preview} type="audio/ogg" />
+              </audio>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 };
 
 export default SingleAlbum;
