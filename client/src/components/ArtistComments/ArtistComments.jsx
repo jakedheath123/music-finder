8b7768f7134d736a8e77e9825527e1ba76f4b4a8
jakedheath123 <jakedheath@gmail.com>
2020-07-02 15:24:26 +0100

    add query to get artist comments data

--- client/src/components/ArtistComments/ArtistComments.jsx
+++ client/src/components/ArtistComments/ArtistComments.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-const ArtistComments = () => {
-  return <div>COMMENTS</div>;
+const ArtistComments = ({ artist_id }) => {
+  const ARTIST_COMMENTS_QUERY = gql`
+  query artistCommentsQuery {
+    artistComments(id: "${artist_id}") {
+      text
+      id
+    }
+  }
+  `;
+
+  const { loading, data } = useQuery(ARTIST_COMMENTS_QUERY);
+
+  if (loading) return <p>Loading..</p>;
+
+  const { artistComments } = data;
+  return (
+    <ul>
+      {artistComments.map(comment => {
+        const { text, id } = comment;
+        return (
+          <li key={id}>
+            <p>" {text} "</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ArtistComments;
