33e2f3b038fb04fdde5670f1e018ddaa2103e0f3
jakedheath123 <jakedheath@gmail.com>
2020-07-02 16:18:37 +0100

    create single slbum component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Router } from "@reach/router";
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
 import SingleArtistPage from "./components/SingleArtistPage/SingleArtistPage";
+import SingleAlbum from "./components/SingleAlbum/SingleAlbum";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router primary={false}>
         <HomePage path="/" />
         <SingleArtistPage path="/artist/:artist_id" />
+        <SingleAlbum path="/album/:album_id" />
       </Router>
     </>
   );
---
+++ client/src/components/SingleAlbum/SingleAlbum.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleAlbum = () => {
+  return <div>ALBUM</div>;
+};
+
+export default SingleAlbum;
