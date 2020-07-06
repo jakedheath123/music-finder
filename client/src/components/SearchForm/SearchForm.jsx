af391a68b6a6b3b13994a251584735b37d9b168e
jakedheath123 <jakedheath@gmail.com>
2020-07-06 20:38:27 +0100

    refactor component

--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -1,38 +1,34 @@
 import React, { useState } from "react";
 
 const SearchForm = ({ searchByArtist }) => {
-  const [userInput, setUserInput] = useState("");
+  const [userQuery, setUserQuery] = useState("");
 
-  const handleChange = event => {
-    const { value } = event.target;
-    setUserInput(value);
-  };
-
-  const handleSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault();
-    const noSpacesInput = userInput.split(" ").join("");
-    searchByArtist(noSpacesInput);
-    setUserInput("");
+    const noSpacesQuery = userQuery.split(" ").join("");
+    searchByArtist(noSpacesQuery);
+    setUserQuery("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div style={{ border: "1px solid black" }}>
-        <input
-          type="text"
-          name="userInput"
-          value={userInput}
-          onChange={handleChange}
-          style={{ border: "none", textAlign: "center" }}
-          placeholder="Search"
-          required
-        />
-
-        <button style={{ padding: "0", border: "0" }}>
-          <i className="fas fa-search"></i>
-        </button>
-      </div>
-    </form>
+    <section>
+      <form onSubmit={onSubmit}>
+        <div style={{ border: "1px solid black" }}>
+          <input
+            type="text"
+            name="userInput"
+            value={userQuery}
+            onChange={event => setUserQuery(event.target.value)}
+            style={{ border: "none", textAlign: "center" }}
+            placeholder="Search"
+            required
+          />
+          <button style={{ padding: "0", border: "0" }}>
+            <i className="fas fa-search"></i>
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
