086e83ce127239142b55ac9907d32aad0f402aff
jakedheath123 <jakedheath@gmail.com>
2020-07-08 17:32:36 +0100

    refactor ArtistTopTracks component

--- client/src/components/ArtistTopTracks/ArtistTopTracks.jsx
+++ client/src/components/ArtistTopTracks/ArtistTopTracks.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
-import "./ArtistTopTracks.css";
 import Loader from "../Loader/Loader";
+import TrackItem from "./TrackItem";
 
 const ArtistTopTracks = ({ artist_id }) => {
   const ARTIST_TOP_TRACKS_QUERY = gql`
@@ -20,36 +20,7 @@ const ArtistTopTracks = ({ artist_id }) => {
 
   const { loading, data } = useQuery(ARTIST_TOP_TRACKS_QUERY);
 
-  if (loading) return <Loader />;
-
-  const { topArtistTracks } = data;
-
-  return (
-    <>
-      <h2>Top Tracks</h2>
-      <ul>
-        {topArtistTracks.map(track => {
-          const { title_short, preview, contributors } = track;
-          return (
-            <li key={title_short}>
-              <h3>{title_short}</h3>
-              <audio controls>
-                <source src={preview} type="audio/ogg" />
-              </audio>
-              {contributors.length > 3 ? (
-                <h4>{contributors[0].name}</h4>
-              ) : (
-                contributors.map((contributor, index) => {
-                  const { name } = contributor;
-                  return <h4 key={index}>{name}</h4>;
-                })
-              )}
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
+  return loading ? <Loader /> : <TrackItem data={data} />;
 };
 
 export default ArtistTopTracks;
