c7c07336d4a69f7e6df859f9a7654d505b63e54c
jakedheath123 <jakedheath@gmail.com>
2020-07-08 18:06:50 +0100

    rename folder

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ChartArtists from "../ChartArtists/ChartArtists";
+import ArtistSearch from "../ArtistSearch/ArtistSearch";
+
+const Home = () => {
+  return (
+    <main>
+      <ChartArtists />
+      <ArtistSearch />
+    </main>
+  );
+};
+
+export default Home;
