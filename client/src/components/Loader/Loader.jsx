c11637336b8029419e998b9285ed9655851baf89
jakedheath123 <jakedheath@gmail.com>
2020-07-08 13:39:30 +0100

    create Loader compoent

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loadingio-spinner-cube-a1drmf4bq2">
+      <div className="ldio-b5x4q7m52cr">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
