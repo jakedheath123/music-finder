69ced8b07919ea28a69ecd0039b5868e2f28c0c2
jakedheath123 <jakedheath@gmail.com>
2020-07-08 17:31:11 +0100

    create TrackItem component

---
+++ client/src/components/ArtistTopTracks/TrackItem.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./TrackItem.css";
+
+const TrackItem = ({ data: { topArtistTracks } }) => {
+  return (
+    <div className="track-item-container">
+      <h2>Top Tracks</h2>
+      <ul>
+        {topArtistTracks.map(track => {
+          const { title_short, preview, contributors } = track;
+          return (
+            <li key={title_short}>
+              <h3>{title_short}</h3>
+              <audio controls>
+                <source src={preview} type="audio/ogg" />
+              </audio>
+              {contributors.length > 3 ? (
+                <h4>{contributors[0].name}</h4>
+              ) : (
+                contributors.map((contributor, index) => {
+                  const { name } = contributor;
+                  return <h4 key={index}>{name}</h4>;
+                })
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default TrackItem;
