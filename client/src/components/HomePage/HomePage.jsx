afc5cae785fc58da33f6029cfcb1c8661d783c38
jakedheath123 <jakedheath@gmail.com>
2020-07-08 13:57:27 +0100

    refactor to return ArtistSearch component

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,49 +1,16 @@
-import React, { useState } from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { gql } from "apollo-boost";
-import { Link } from "@reach/router";
+import React from "react";
 
 import "./HomePage.css";
-import SearchForm from "../SearchForm/SearchForm";
 import ChartArtistsList from "../ChartArtistsList/ChartArtistsList";
-import Loader from "../Loader/Loader";
+import ArtistSearch from "../ArtistSearch/ArtistSearch";
 
 const HomePage = () => {
-  const [artist, setArtist] = useState("");
-
-  const ARTIST_QUERY = gql`
-    query artistQuery {
-      artist(name: "${artist}") {
-        name
-        id
-        picture_big
-      }
-    }
-  `;
-
-  const { loading, data } = useQuery(ARTIST_QUERY);
-
-  if (loading) return <Loader />;
-
-  const { name, picture_big, id } = data.artist;
-
   return (
     <main className="home-container">
       <section className="home-chart-artists">
         <ChartArtistsList />
       </section>
-      <SearchForm
-        searchByArtist={query => setArtist(query)}
-        className="home-search"
-      />
-      <section className="home-content">
-        {picture_big ? (
-          <Link to={`/artist/${id}`}>
-            <img src={picture_big} alt="Artist cover" />
-          </Link>
-        ) : null}
-        <h1>{name}</h1>
-      </section>
+      <ArtistSearch />
     </main>
   );
 };
