eec0d057c1ebcc1d9d9b145cf545487325a8940e
jakedheath123 <jakedheath@gmail.com>
2020-07-08 16:58:00 +0100

    rename folder

---
+++ client/src/components/SingleArtist/SingleArtist.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+import Loader from "../Loader/Loader";
+import SingleArtistDetails from "./SingleArtistDetails";
+
+const ArtistPage = ({ artist_id }) => {
+  const ARTIST_QUERY = gql`
+    query artistQuery {
+      artist(name: "${artist_id}") {
+       name
+       picture_big
+   }
+ }
+ `;
+
+  const { loading, data } = useQuery(ARTIST_QUERY);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <SingleArtistDetails data={data} artist_id={artist_id} />
+  );
+};
+
+export default ArtistPage;
---
+++ client/src/components/SingleArtist/SingleArtistDetails.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+import "./SingleArtistDetails.css";
+import ArtistTopTracks from "../ArtistTopTracks/ArtistTopTracks";
+import ArtistAlbums from "../ArtistAlbums/ArtistAlbums";
+import ArtistComments from "../ArtistComments/ArtistComments";
+
+const SingleArtistDetails = ({
+  data: {
+    artist: { name, picture_big }
+  },
+  artist_id
+}) => {
+  return (
+    <main className="single-artist-container">
+      <section className="single-artist-bio">
+        <Link to="/">
+          <button>Back</button>
+        </Link>
+        <h1>{name}</h1>
+        <img src={picture_big} alt="Artist cover" />
+      </section>
+      <section className="single-artist-tracks">
+        <ArtistTopTracks artist_id={artist_id} />
+      </section>
+      <section className="single-artist-albums">
+        <ArtistAlbums artist_id={artist_id} />
+      </section>
+      <section className="single-artist-comments">
+        <ArtistComments artist_id={artist_id} />
+      </section>
+    </main>
+  );
+};
+
+export default SingleArtistDetails;
