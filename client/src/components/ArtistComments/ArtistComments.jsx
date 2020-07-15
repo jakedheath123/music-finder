4fef7934d0483c090dc2f25839b91e085a0bfad0
jakedheath123 <jakedheath@gmail.com>
2020-07-15 17:25:03 +0100

    import commentCheck function to use on artist comments component

--- client/src/components/ArtistComments/ArtistComments.jsx
+++ client/src/components/ArtistComments/ArtistComments.jsx
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/react-hooks";
 
 import Loader from "../Loader/Loader";
 import "./ArtistComments.css";
+import { commentCheck } from "../../utils/utils";
 
 const ArtistComments = ({ artist_id }) => {
   const ARTIST_COMMENTS_QUERY = gql`
@@ -21,7 +22,7 @@ const ArtistComments = ({ artist_id }) => {
     <Loader />
   ) : (
     <ul className="artist-comments">
-      {data.artistComments.map(comment => {
+      {commentCheck(data.artistComments).map(comment => {
         const { text, id } = comment;
         return (
           <li key={id}>
