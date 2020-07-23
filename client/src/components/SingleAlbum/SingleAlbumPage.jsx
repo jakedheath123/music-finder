f9d097adfd9851a1ed9999fcf527377615d2ad6a
jakedheath123 <jakedheath@gmail.com>
2020-07-23 20:35:40 +0100

    udpate component name

--- client/src/components/SingleAlbum/SingleAlbumPage.jsx
+++ client/src/components/SingleAlbum/SingleAlbumPage.jsx
@@ -2,27 +2,38 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
-import SingleAlbumTrack from "./SingleAlbumTrack";
+import SingleAlbumDetails from "./SingleAlbumDetails";
 import Loader from "../Loader/Loader";
 
 const SingleAlbum = ({ album_id }) => {
-  const ALBUM_TRACKS_QUERY = gql`
-  query albumTracksQuery {
-    albumTracks(id: ${album_id}) {
-      id
-      title_short
-      track_position
-      preview
+  const SINGLE_ALBUM_QUERY = gql`
+  query singleAlbumQuery {
+    singleAlbum(id: ${album_id}) {
+      title
+      cover_medium
+      release_date
+      explicit_lyrics
+      fans
       artist {
+        name
+        picture_big
         id
+      }
+      tracks {
+        data {
+          title_short
+          explicit_lyrics
+          preview
+          id
+        }
       }
     }
   }
   `;
 
-  const { loading, data } = useQuery(ALBUM_TRACKS_QUERY);
+  const { loading, data } = useQuery(SINGLE_ALBUM_QUERY);
 
-  return loading ? <Loader /> : <SingleAlbumTrack data={data} />;
+  return loading ? <Loader /> : <SingleAlbumDetails data={data} />;
 };
 
 export default SingleAlbum;
